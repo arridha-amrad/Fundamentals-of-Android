51ef518f79c8c1685063ef1a9557c9726d1eb4fd
arridhaamrad <arridhaamrad@gmail.com>
2021-02-17 22:30:19 +0700

    Fragments

---
+++ app/src/main/java/com/example/androidfundametals/FirstFragment.kt
@@ -0,0 +1,13 @@
+package com.example.androidfundametals
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [FirstFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class FirstFragment : Fragment(R.layout.fragment_first) {
+}
--- app/src/main/java/com/example/androidfundametals/MainActivity.kt
+++ app/src/main/java/com/example/androidfundametals/MainActivity.kt
@@ -1,53 +1,44 @@
 package com.example.androidfundametals
 
-import android.content.Context
-import android.os.Build
 import android.os.Bundle
-import android.view.inputmethod.InputMethodManager
-import android.widget.Toast
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.androidfundametals.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
   private lateinit var binding: ActivityMainBinding;
 
-  @RequiresApi(Build.VERSION_CODES.M)
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     binding = ActivityMainBinding.inflate(layoutInflater)
     val view = binding.root;
     setContentView(view)
 
-    val todoList = mutableListOf<Todo>(
-      Todo("eat with egg", false),
-      Todo("go shopping", true),
-      Todo("debugging e-commerce android", false),
-      Todo("meeting with mr.James", false)
-    )
+    val firstFragment = FirstFragment()
+    val secondFragment = SecondFragment()
 
-    val adapter = TodoAdapter(todoList)
-    binding.rvTodo.adapter = adapter
-    binding.rvTodo.layoutManager = LinearLayoutManager(this)
-
-    binding.apply {
-      btnAdd.setOnClickListener {
-        val title = etTodo.text.toString()
-        val newTodo = Todo(title, false)
-        todoList.add(0, newTodo)
-        adapter.notifyItemInserted(0)
-        etTodo.text = null
-        Toast.makeText(this@MainActivity, "New todo added", Toast.LENGTH_LONG).show()
-
-        // dismiss the keyboard
-        val imm: InputMethodManager =
-          getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(etTodo.getWindowToken(), 0)
-      }
+    supportFragmentManager.beginTransaction().apply {
+      replace(R.id.flFragment, firstFragment)
+      commit()
     }
 
+    binding.apply {
+      btnFragment1.setOnClickListener {
+        supportFragmentManager.beginTransaction().apply {
+          replace(R.id.flFragment, firstFragment)
+          commit()
+        }
+      }
+
+      btnFragment2.setOnClickListener {
+        supportFragmentManager.beginTransaction().apply {
+          replace(R.id.flFragment, secondFragment)
+          // let you go back to prev fragment
+          addToBackStack(null)
+          commit()
+        }
+      }
+    }
   }
 
 }
