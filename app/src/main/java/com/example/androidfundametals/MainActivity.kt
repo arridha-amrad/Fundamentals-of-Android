5a58fd9ebe352ce7c0235ba85399718924a22e0a
arridhaamrad <arridhaamrad@gmail.com>
2021-02-21 17:52:15 +0700

    Tab layout with viewpager2

--- app/src/main/java/com/example/androidfundametals/MainActivity.kt
+++ app/src/main/java/com/example/androidfundametals/MainActivity.kt
@@ -2,39 +2,55 @@ package com.example.androidfundametals
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.example.androidfundametals.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding;
 
-  private lateinit var binding: ActivityMainBinding;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root;
+        setContentView(view)
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    binding = ActivityMainBinding.inflate(layoutInflater)
-    val view = binding.root;
-    setContentView(view)
+        val images = listOf(
+                R.drawable.img1,
+                R.drawable.img2,
+                R.drawable.img3,
+                R.drawable.img4,
+                R.drawable.img5,
+                R.drawable.img6,
+        )
 
-    val images = listOf<Int>(
-        R.drawable.img1,
-        R.drawable.img2,
-        R.drawable.img3,
-        R.drawable.img4,
-        R.drawable.img5,
-        R.drawable.img6,
-    )
-    val vpAdapter = ViewPagerAdapter(images)
-    binding.viewPager.apply {
-      adapter = vpAdapter
-      orientation = ViewPager2.ORIENTATION_VERTICAL
+        val vpAdapter = ViewPagerAdapter(images)
+        binding.viewPager.apply {
+            adapter = vpAdapter
+        }
 
-      beginFakeDrag()
-      fakeDragBy(-10f)
-      endFakeDrag()
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            tab.text = "Tab ${position + 1}" }.attach()
+
+        binding.tabLayout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@MainActivity, "Selected tab ${tab?.text}", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@MainActivity, "UnSelected tab ${tab?.text}", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                Toast.makeText(this@MainActivity, "Reselected tab ${tab?.text}", Toast.LENGTH_SHORT).show()
+            }
+
+        })
+
     }
-
-  }
 
 }
